0023bb11fa3bb066f012df6e4453571bfdc9da94
Nikila99gimhan <nikila99fernando.gimhan@gmail.com>
2022-11-30 17:07:40 +0530

    groovy creds

--- script.groovy
+++ script.groovy
@@ -5,10 +5,10 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nikila99/demo-app:jma-2.0 .'
+    withCredentials([usernamePassword(credentialsId: 'Dockerhub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t nikila99/java-maven-app:jma-2.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nikila99/demo-app:jma-2.0'
+        sh 'docker push nikila99/java-maven-app:jma-2.0'
     }
 } 
 
